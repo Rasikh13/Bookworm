5f4d742a66d881089b872e015f891cf5abb7e793
gauravgavhane <gauravgavhane1702@gmail.com>
2026-08-03 15:11:51 +0530

    Revert "just conflict resolved"
    This reverts commit 8999881deacc2f31bc288e4f3662f84e028ab85d.

--- bookworm-backend-scheduler - BackendWorking/bookworm-backend-scheduler - Copy/src/main/java/com/bookworm/backend/repository/CreditTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bookworm.backend.repository;
-
-import com.bookworm.backend.entity.CreditType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditTypeRepository extends JpaRepository<CreditType, Long> {
-    boolean existsByCreditTypeNameIgnoreCase(String creditTypeName);
-}
